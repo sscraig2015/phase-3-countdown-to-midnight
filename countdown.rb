cc454c7e0a6338fa15a3e8e60123ff25a460cade
Sean <craig.sean92@gmail.com>
2022-04-06 21:03:32 -0600

    commit tree 110dba570f1bb1c20175924e659de71b9a26920e

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
 
-def countdown
+require "pry"
+
+def countdown (i)
+    while i >= 1 do
+        puts "#{i} SECOND(S)!"
+        i -= 1
+    end
+    if i < 1 
+        return "HAPPY NEW YEAR!"
+    end
+end
+
+def countdown_with_sleep (i)
+    while i >= 1 do
+        puts "#{i} SECOND(S)!"
+        i -= 1
+        sleep 1
+    end
+    if i < 1 
+        return "HAPPY NEW YEAR!"
+    end
 end
